d64a8bcfc9aaa02c815cc2770a75a238450fc08b
Matthew Wooldridge <mwooldridge@ultraxinc.com>
2025-08-05 16:23:19 -0500

    Initial

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- PLUGINS
+require("config.lazy")
+require('hardline').setup {}
+
+vim.diagnostic.config({ virtual_text = true })
+
+-- THEME STUFF
+vim.cmd [[colorscheme zephyr]]
+vim.opt.number = true
+
+-- KEYMAPS
+--	LEADER + p = telescope find file names
+vim.api.nvim_set_keymap('n', '<leader>p', ':lua require"telescope.builtin".find_files{}<CR>',
+	{ noremap = true, silent = true })
+--	LEADER + SHIFT + p = telescope grep search directory recursively
+vim.api.nvim_set_keymap('n', '<leader>P', ':lua require"telescope.builtin".live_grep{}<CR>',
+	{ noremap = true, silent = true })
+--	LEADER + r = rename symbol neovim
+vim.api.nvim_set_keymap('n', '<leader>r', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+--	LEADER + f = format buffer
+vim.api.nvim_set_keymap('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true });
---
+++ lua/plugins/zephyr-theme.lua
@@ -0,0 +1,4 @@
+return {
+	'glepnir/zephyr-nvim',
+	requires = { 'nvim-treesitter/nvim-treesitter', opt = true }
+}
