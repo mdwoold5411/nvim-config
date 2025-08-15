8edde59b353bbd588da42074550f234dd8329ea1
Matthew Wooldridge <mwooldridge@ultraxinc.com>
2025-08-15 16:08:17 -0500

    e

--- init.lua
+++ init.lua
@@ -1,71 +1,57 @@
+---@diagnostic disable: undefined-global
 -- PLUGINS
 require("config.lazy")
-require('hardline').setup {}
-require('kanagawa').setup({
-overrides = function(colors)
-    local theme = colors.theme
-    return {
-        TelescopeTitle = { fg = theme.ui.special, bold = true },
-        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
-        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-        PmenuSbar = { bg = theme.ui.bg_m1 },
-        PmenuThumb = { bg = theme.ui.bg_p2 },
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        FloatTitle = { bg = "none" },
-
-        -- Save an hlgroup with dark background and dimmed foreground
-        -- so that you can use it where your still want darker windows.
-        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-        -- Popular plugins that open floats will link to NormalFloat by default;
-        -- set their background accordingly if you wish to keep them dark and borderless
-        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-    }
-end,
-})
-require'marks'.setup {
-  -- whether to map keybinds or not. default true
-  default_mappings = true,
-  -- which builtin marks to show. default {}
-  builtin_marks = { ".", "<", ">", "^" },
-  -- whether movements cycle back to the beginning/end of buffer. default true
-  cyclic = true,
-  -- whether the shada file is updated after modifying uppercase marks. default false
-  force_write_shada = false,
-  -- how often (in ms) to redraw signs/recompute mark positions. 
-  -- higher values will have better performance but may cause visual lag, 
-  -- while lower values may cause performance penalties. default 150.
-  refresh_interval = 250,
-  -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-  -- marks, and bookmarks.
-  -- can be either a table with all/none of the keys, or a single number, in which case
-  -- the priority applies to all marks.
-  -- default 10.
-  sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-  -- disables mark tracking for specific filetypes. default {}
-  excluded_filetypes = {},
-  -- disables mark tracking for specific buftypes. default {}
-  excluded_buftypes = {},
-  -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-  -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-  -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-  -- default virt_text is "".
-  mappings = {}
+require 'config.marks-setup'
+require 'config.gitsigns-setup'
+require 'git-conflict'.setup {
+	default_mappings = {
+		ours = 'o',
+		theirs = 't',
+		none = '0',
+		both = 'b',
+		next = 'n',
+		prev = 'p',
+	},
 }
 
+-- MINI STUFF
+require('mini.hues').setup({
+	background = '#101010',
+	foreground = '#ffffff',
+	saturation = 'medium',
+	n_hues = 8,
+	accent = 'purple',
+	plugins = { default = true },
+})
+require('mini.ai').setup()
+require('mini.surround').setup()
+require('mini.operators').setup()
+require('mini.pairs').setup()
+require('mini.icons').setup()
+require('mini.completion').setup()
+local gen_loader = require('mini.snippets').gen_loader
+require('mini.snippets').setup({
+	snippets = {
+		-- Load custom file with global snippets first (adjust for Windows)
+		gen_loader.from_file('C:\\Users\\mwooldridge\\AppData\\Local\\nvim\\snippets\\global.json'),
+
+		-- Load snippets based on current language by reading files from
+		-- "snippets/" subdirectories from 'runtimepath' directories.
+		gen_loader.from_lang(),
+	},
+})
+require('mini.statusline').setup()
+require('mini.tabline').setup()
+require('mini.bracketed').setup()
+require('mini.files').setup()
+
+
+
+-- NVIM SETTINGS
 vim.diagnostic.config({ virtual_text = true })
 
 -- THEME STUFF
-vim.cmd("colorscheme kanagawa-dragon");
+vim.cmd.colorscheme('miniwinter');
 vim.opt.number = true
 
 -- KEYMAPS
@@ -79,8 +65,9 @@ vim.api.nvim_set_keymap('n', '<leader>P', ':lua require"telescope.builtin".live_
 vim.api.nvim_set_keymap('n', '<leader>r', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
 --	LEADER + f = format buffer
 vim.api.nvim_set_keymap('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true });
---	LEADER + e = File Explorer (CHADtree)
-vim.api.nvim_set_keymap('n', '<leader>e', ':CHADopen<CR>', {silent = true, noremap = true })
+--	LEADER + e = File Explorer (mini-files)
+vim.api.nvim_set_keymap('n', '<leader>e', ':lua MiniFiles.open()<CR>', { silent = true, noremap = true })
 --	LEADER + . = Code Actions (LSP)
-vim.api.nvim_set_keymap('n', '<leader>.', ':lua vim.lsp.buf.code_action()<CR>', {noremap = true, silent = true });
-
+vim.api.nvim_set_keymap('n', '<leader>.', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true });
+--	LEADER + gg = lazygit
+vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,4 @@
+return {
+  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+}
