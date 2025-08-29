34f083d1a7b433f5f9a129ecc0e5dce1f947aaa6
Matthew Wooldridge <mwooldridge@ultraxinc.com>
2025-08-29 14:52:40 -0500

    begin working on open script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 # Setup repo
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -19,7 +19,7 @@ echo \
 sudo apt-get update
 
 # install packages
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 # build image
--- nvim.sh
+++ nvim.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -it nvim-config nvim $@
+if [[ -e $0 ]]; then
+
+else
+
+sudo docker run -it nvim-config nvim 
